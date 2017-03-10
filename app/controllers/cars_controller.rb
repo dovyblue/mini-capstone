6a44a70496232e87069dc84043c65c8b36549c41
Dov Bluming <dovybluming@gmail.com>
2017-03-10 01:41:36 -0500

    implemented index action, show action, changed html file names, made title from the index link to there show pge

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,12 @@
 class CarsController < ApplicationController
-  def dcars
+  def index
     @cars = Car.all
-    render 'dovycars.html.erb'
+    render 'index.html.erb'
+  end
+
+  def show
+    input_id = params[:id]
+    @car = Car.find_by(id: input_id)
+    render 'show.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get '/dovycars' => 'cars#dcars'
+  get '/cars' => 'cars#index'
+  get '/cars/:id' => 'cars#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
