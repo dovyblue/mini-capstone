e4b5529f90ae0e6b48b5818c34ac750fcc963fc5
Dov Bluming <dovybluming@gmail.com>
2017-03-14 16:21:00 -0400

    REST and style

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,12 +1,58 @@
 class CarsController < ApplicationController
+  
   def index
     @cars = Car.all
     render 'index.html.erb'
   end
 
+  def new 
+    render 'new.html.erb'
+  end
+
+  def create
+    @car = Car.new(
+      make: params['form_make'],
+      model: params['form_model'],
+      year: params['form_year'],
+      price: params['form_price'],
+      image: params['form_image'],
+      description: params['form_description']
+    )
+    @car.save
+    flash[:success] = "you have submitted"
+    redirect_to "/cars/#{@car.id}"
+  end
+
   def show
     input_id = params[:id]
-    @car = Car.find_by(id: input_id)
+    @car = Car.find(input_id)
     render 'show.html.erb'
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(
+      make: params['form_make'],
+      model: params['form_model'],
+      year: params['form_year'],
+      price: params['form_price'],
+      image: params['form_image'],
+      description: params['form_description']
+    )
+    flash[:info] = "nice update!"
+    redirect_to "/cars/#{@car.id}"
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @id = @car.id
+    @car.destroy
+    flash[:danger] = "yoishhh... it's deleted"
+    redirect_to '/cars'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get '/cars' => 'cars#index'
+  get '/cars/new' => 'cars#new'
+  post '/cars' => 'cars#create'
   get '/cars/:id' => 'cars#show'
+  get '/cars/:id/edit' => 'cars#edit'
+  patch '/cars/:id' => 'cars#update'
+  delete '/cars/:id' => 'cars#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
