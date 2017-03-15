9f7475bc4de89f8c581544e0da242a58e90d6dd2
Dov Bluming <dovybluming@gmail.com>
2017-03-14 23:45:42 -0400

    model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,15 @@
 class Car < ApplicationRecord
+  def sale_message
+    return "Discount item!" if price <= 20000
+    return "Everyday value!" if price > 20000
+  end
+
+  def tax
+    tax_amount = price * 0.09
+    return tax_amount.to_i
+  end
+
+  def total
+    price + tax
+  end
 end
